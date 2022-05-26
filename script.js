066ccc675f966ce4c2ac1aabe10ea10dd687eb63
prudhvidendukuri <104501345+prudhvidendukuri@users.noreply.github.com>
2022-05-26 21:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function algebra() {
+    var one = document.getElementById('text1').value;
+    let a = parseFloat(one)
+    var two = document.getElementById('text2').value;
+    let b = parseFloat(two);
+    var sum = a + b;
+    var product = a * b;
+    var divison = a / b;
+    var sub = a - b;
+    document.getElementById('sum').value = sum;
+    document.getElementById('product').value = product;
+    document.getElementById('divison').value = divison;
+    document.getElementById('sub').value = sub;
+}
